f4a5aea72878904c05eb9842d30367c343cb8127
Noah Meyerhans <noahm@debian.org>
2022-10-06 12:11:29 -0700

    Don't install route config overrides when using the main table
    After a8580fa2157f ("Skip routing rules for default mac interface"),
    we no longer create a secondary routing table for the primary
    interface, so we no longer need to emit configuration to install
    routes into that table.  Following this change, the primary ENI (that
    with device-number=0) will get an eni.conf similar to:
    
    [Match]
    MACAddress=06:4b:8d:43:93:59
    [Network]
    DHCP=yes
    [DHCPv4]
    RouteMetric=512
    [DHCPv6]
    RouteMetric=512
    
    While a secondary ENI would continue to get additional stanzas
    creating routes in the secondary routing table, as in:
    
    [Match]
    MACAddress=06:8a:d8:09:9c:bf
    [Network]
    DHCP=yes
    [DHCPv4]
    RouteMetric=513
    [DHCPv6]
    RouteMetric=513
    [Route]
    Table=10001
    Gateway=_ipv6ra
    [DHCPv4]
    RouteTable=10001
    [IPv6AcceptRA]
    RouteTable=10001
    [Route]
    Gateway=_dhcp4
    Table=10001

--- lib/lib.sh
+++ lib/lib.sh
@@ -252,28 +252,28 @@ DHCP=yes
 RouteMetric=${metric}
 [DHCPv6]
 RouteMetric=${metric}
-[Route]
-Table=${tableid}
-Gateway=_ipv6ra
 EOF
 
     if [ "$tableid" -gt 0 ]; then
         cat <<EOF >> "${dropin}.tmp"
+[Route]
+Table=${tableid}
+Gateway=_ipv6ra
 [DHCPv4]
 RouteTable=${tableid}
 [IPv6AcceptRA]
 RouteTable=${tableid}
 EOF
-    fi
-
-    if subnet_supports_ipv4 "$iface"; then
-        # if we're not in a v6-only network, add IPv4 routes to the private table
-        cat <<EOF >> "${dropin}.tmp"
+	if subnet_supports_ipv4 "$iface"; then
+            # if we're not in a v6-only network, add IPv4 routes to the private table
+            cat <<EOF >> "${dropin}.tmp"
 [Route]
 Gateway=_dhcp4
 Table=${tableid}
 EOF
+	fi
     fi
+
     mv "${dropin}.tmp" "$dropin"
     echo 1
 }
